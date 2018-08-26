e3cafeaf1fbaffc6875c77ee6a5ffcc1db8de68b
Ouyangqingxing <1119643931@qq.com>
2018-08-26 12:40:19 +0800

    更新笔记和代码

--- resources/js/end.js
+++ resources/js/end.js
@@ -3,16 +3,16 @@
  * 全局变量
  * 句子总数，当前句子序号，主题序号
  */
-var sentenceNumber = 20;
-var currentSentence = 1;
+var sentenceNumber = 25;
+var currentSentence = -1;
 
 $(document).ready(function()
 {
     // 兜底逻辑
     $("#rightLoading").show();
-    var defalut = window.setTimeout("hideDefalut()",3000);
+    var defalut = window.setTimeout("hideDefalut()",2000);
     // 定时随机换一个句子
-    var sentenceTime = window.setInterval("displaySentence()",3200); 
+    var sentenceTime = window.setInterval("displaySentence()",2500); 
 });
 
 /**
@@ -31,9 +31,17 @@ function displaySentence()
 {
     // 隐藏当前展示的句子
     $("#p" + currentSentence).hide();
-    // 获取一个随机数
-    var random = Math.floor(Math.random() * (20) + 1);
+    // 获取下一个句子
+    var next = currentSentence + 2;
+    if(next > sentenceNumber){
+        if(next % 2 == 1){
+            next = 2;
+        }
+        else{
+            next = 1;
+        }
+    }
     // 展示这个句子，并修改变量
-    $("#p" + random).show();
-    currentSentence = random;
+    $("#p" + next).show();
+    currentSentence = next;
 }
